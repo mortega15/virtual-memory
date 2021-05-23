97c0437a3eb73c9126693190d15223a206fbe4c3
mortega15 <79822470+mortega15@users.noreply.github.com>
2021-05-22 19:03:38 -0700

    Update memmgr.c

--- memmgr.c
+++ memmgr.c
@@ -28,6 +28,11 @@ int pfc[5], pfc2[5]; // page fault count
 int tlbh[5], tlbh2[5]; // tlb hit count
 int count[5], count2[5]; // access count
 
+
+int pfc_prev_hit = 0;
+int tlb_prev_hit = 0;
+
+
 #define PAGES 256
 #define FRAMES_PART1 256
 #define FRAMES_PART2 128
@@ -44,71 +49,105 @@ void getpage_offset(unsigned x) {
          (page << 8) | getoffset(x), page * 256 + offset);
 }
 
-int tlb_contains(unsigned x) {  // TODO:
-   
-
+int tlb_contains(unsigned x) {
+  for (int i = 0; i < 16; i++){
+    if (tlb[i][0] == x) { return i; }
+  }
   return -1;
 }
 
 void update_tlb(unsigned page) {
-    page_table[page] = true;
+  tlb[current_tlb_entry][0] = page;
+  tlb[current_tlb_entry][1] = page_table[page];
+  current_tlb_entry = (current_tlb_entry+1) % 16; // round-robin
 }
 
-unsigned getframe(FILE* fstore, unsigned logic_add, unsigned page, int *page_fault_count, int *tlb_hit_count) 
-{              // TODO
-  // tlb hit
+unsigned getframe(FILE* fstore, unsigned logic_add, unsigned page,
+         int *page_fault_count, int *tlb_hit_count) {
 
-  
-  
+  int tlb_index = tlb_contains(page);
+  if (tlb_index != -1) {
+    (*tlb_hit_count)++;
+    return tlb[tlb_index][1];
+  }
+
   // tlb miss
   // if page table hit
+  if (page_table[page] != -1) {
+    update_tlb(page);
+    return page_table[page];
+  }
 
-  
   // page table miss -> page fault
   // find page location in backing_store
+  int offset = (logic_add / FRAME_SIZE) * FRAME_SIZE;
+  fseek(fstore, offset, 0);
 
-  
-  
   // bring data into memory, update tlb and page table
-
-
+  page_table[page] = current_frame;
+  current_frame = (current_frame + 1) % 256;
+  (*page_fault_count)++;
+  fread(&main_mem[page_table[page] * FRAME_SIZE], sizeof(char), 256, fstore);
+  update_tlb(page);
+  return page_table[page];
 }
 
-int get_available_frame(unsigned page) {    // TODO
+int get_available_frame(unsigned page) {
   // empty queue
-    page_queue[page] == NULL;
-    page_queue[page] == NULL;
-  
+  if (qhead == 0 && qtail == 0 && page_queue[qhead] == -1){
+    ++qtail;
+    page_queue[qhead] = page;
+    return qhead;
+  }
+
   // queue not full
-    page_queue[page] > 128;
-  
+  if (page_queue[qtail] == -1) {
+    page_queue[qtail] = page;
+    int val = qtail;
+    qtail = (qtail + 1) % 128;
+    return val;
+  }
+
   // queue full
-    page_queue[page] == 128;
-
-
+  if (qhead == qtail && page_queue[qtail] != -1) {
+    page_queue[qhead] = page;
+    int val = qhead;
+    qhead = (qhead + 1) % 128;
+    qtail = (qtail + 1) % 128;
+    return val;
+  }
   return -1;   // failed to find a value
 }
 
 unsigned getframe_fifo(FILE* fstore, unsigned logic_add, unsigned page,
          int *page_fault_count, int *tlb_hit_count) {
   // tlb hit
-
-
+  int tlb_index = tlb_contains(page);
+  if (tlb_index != -1) {
+    if (page_queue[tlb[tlb_index][1]] == page) {
+      (*tlb_hit_count)++;
+      return tlb[tlb_index][1];
+    }
+  }
 
   // tlb miss, page table hit
- 
-  
-  
-  
+  if (page_table[page] !=- 1 && page_queue[page_table[page]] == page) {
+    update_tlb(page);
+    return page_table[page];
+  }
+
   // page table miss -> page fault
   // find location in backing_store
+  int offset = (logic_add / FRAME_SIZE) * FRAME_SIZE;
+  fseek(fstore, offset, 0);
 
-  
-  
-  
   // bring data into memory, update tlb and page table
-
-
+  int available_frame = get_available_frame(page);
+  fread(&main_mem_fifo[available_frame*FRAME_SIZE], sizeof(char), 256, fstore);
+  page_table[page] = available_frame;
+  (*page_fault_count)++;
+  update_tlb(page);
+  return page_table[page];
 }
 
 void open_files(FILE** fadd, FILE** fcorr, FILE** fstore) {
@@ -136,18 +175,17 @@ void simulate_pages_frames_equal(void) {
   unsigned   logic_add;                  // read from file address.txt
   unsigned   virt_add, phys_add, value;  // read from file correct.txt
 
-
   FILE *fadd, *fcorr, *fstore;
   open_files(&fadd, &fcorr, &fstore);
-  
+
   // Initialize page table, tlb
   memset(page_table, -1, sizeof(page_table));
   for (int i = 0; i < 16;  ++i) { tlb[i][0] = -1; }
-  
+
   int access_count = 0, page_fault_count = 0, tlb_hit_count = 0;
   current_frame = 0;
   current_tlb_entry = 0;
-  
+
   printf("\n Starting nPages == nFrames memory simulation...\n");
 
   while (fscanf(fadd, "%d", &logic_add) != EOF) {
@@ -170,9 +208,13 @@ void simulate_pages_frames_equal(void) {
       pfc[(access_count / 200) - 1] = page_fault_count;
       count[(access_count / 200) - 1] = access_count;
     }
-    
-    printf("logical: %5u (page: %3u, offset: %3u) ---> physical: %5u -> value: %4d  ok\n", logic_add, page, offset, physical_add, val);
+
+    printf("logical: %5u (page: %3u, offset: %3u) ---> physical: %5u -> value: %4d  ok  #hits: %d  %d  %s  %s\n", logic_add, page, offset, physical_add, val, page_fault_count, tlb_hit_count, (page_fault_count > pfc_prev_hit ? "PgFAULT" : ""), (tlb_hit_count > tlb_prev_hit)? "         TLB HIT" : "");
     if (access_count % 5 ==  0) { printf("\n"); }
+    if (access_count % 50 == 0) { printf("========================================================================================================== %d\n\n", access_count); }
+
+    pfc_prev_hit = page_fault_count;
+    tlb_prev_hit = tlb_hit_count;
 
     assert(physical_add ==  phys_add);
     assert(value ==  val);
@@ -180,7 +222,7 @@ void simulate_pages_frames_equal(void) {
   fclose(fcorr);
   fclose(fadd);
   fclose(fstore);
-  
+
   printf("ALL logical ---> physical assertions PASSED!\n");
   printf("ALL read memory value assertions PASSED!\n");
 
@@ -200,7 +242,7 @@ void simulate_pages_frames_not_equal(void) {
   memset(page_table, -1, sizeof(page_table));
   memset(page_queue, -1, sizeof(page_queue));
   for (int i = 0; i < 16;  ++i) { tlb[i][0] = -1; }
-  
+
   int access_count = 0, page_fault_count = 0, tlb_hit_count = 0;
   qhead = 0; qtail = 0;
 
@@ -227,20 +269,19 @@ void simulate_pages_frames_not_equal(void) {
       pfc2[(access_count / 200) - 1] = page_fault_count;
       count2[(access_count / 200) - 1] = access_count;
     }
-    
-  //  printf("logical: %5u (page: %3u, offset: %3u) ---> physical: %5u -> value: %4d  ok\n", logic_add, page, offset, physical_add, val);
-   // if (access_count % 5 ==  0) { printf("\n"); 
 
-    printf("logical: % 5u (page : % 3u, offset : % 3u)--->physical : % 5u->value : % 4d ok #hits : % d % d % s % s\n", 
-        logic_add, page, offset, physical_add, val, page_fault_count, tlb_hit_count,
-        (page_fault_count > pfc_prev_hit ? "PgFAULT" : ""), (tlb_hit_count > tlb_prev_hit) ? "     TLB HIT" : "");
-    if (access_count % 5 == 0) { printf("\n"); }
-    if (access_count % 50 == 0) { printf("========================================================================================================== % d\n\n", access_count); }
-    
-    pfc_prev_hit = page_fault_count;                  // new global variable — put at top of file, and initialize to 0
-    tlb_prev_hit = tlb_hit_count;                     // new global variable — put at top of file, and initialize to 0
+//    printf("logical: %5u (page: %3u, offset: %3u) ---> physical: %5u -> value: %4d  ok\n", logic_add, page, offset, physical_add, val);
+//    if (access_count % 5 ==  0) { printf("\n"); }
+    printf("logical: %5u (page: %3u, offset: %3u) ---> physical: %5u -> value: %4d  ok  #hits: %d  %d  %s  %s\n", logic_add, page, offset, physical_add, val, page_fault_count, tlb_hit_count,
+           (page_fault_count > pfc_prev_hit ? "PgFAULT" : ""),
+           (tlb_hit_count > tlb_prev_hit)   ? "         TLB HIT" : "");
+    if (access_count % 5 ==  0) { printf("\n"); }
+    if (access_count % 50 == 0) { printf("========================================================================================================== %d\n\n", access_count); }
 
-    assert(value == val);
+    pfc_prev_hit = page_fault_count;
+    tlb_prev_hit = tlb_hit_count;
+
+    assert(value ==  val);
   }
   close_files(fadd, fcorr, fstore);
 
@@ -248,11 +289,12 @@ void simulate_pages_frames_not_equal(void) {
   printf("\n\t\t... nPages != nFrames memory simulation done.\n");
 }
 
-
 int main(int argc, const char* argv[]) {
   // initialize statistics data
   for (int i = 0; i < 5; ++i){
-    pfc[i] = pfc2[i] = tlbh[i]  = tlbh2[i] = count[i] = count2[i] = 0;
+    pfc[i]   = pfc2[i] = 0;
+    tlbh[i]  = tlbh2[i] = 0;
+    count[i] = count2[i] = 0;
   }
 
   simulate_pages_frames_equal(); // 256 physical frames
